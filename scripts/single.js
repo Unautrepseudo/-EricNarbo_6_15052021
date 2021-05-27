8757df92958584a803dc51c90b73e9cb96f05db1
Eric <eric.narbo@gmail.com>
2021-05-28 00:21:57 +0200

    display medias

--- scripts/single.js
+++ scripts/single.js
@@ -13,7 +13,10 @@ const displaySingleInfo = () => {
   infos.map(({ name, id, city, country, tags, tagline, price, portrait }) => {
     herosInfos.innerHTML = `
       <div class="single-card">
-        <h1>${name}</h1>
+        <div class="name-container">
+          <h1>${name}</h1>
+          <button>Contactez-moi</button>
+        </div>
         <div class="sc-location">
           <span class="city">${city}</span>
           <span class="country">${country}</span>
@@ -31,6 +34,34 @@ const displaySingleInfo = () => {
     `;
   });
   heroImg.setAttribute('src', `./assets/img/idPhotos/${infos[0].portrait}`);
-  console.log(infos.portrait);
 };
 displaySingleInfo();
+
+const displayMedia = () => {
+  let singleName = infos[0].name;
+  medias.map(({ title, image, likes, id }) => {
+    const like = +likes;
+    console.log(like);
+    singleContentContainer.innerHTML += `
+      <div class="sc-card">
+        <div id='${id}' class="img-container">
+          <img src="./assets/img/${singleName}/${image}" alt="une belle image" />
+        </div>
+        <div class="sc-card-footer">
+          <span class="sc-card-title">${title}</span>
+          <div class="likes-container">
+            <span class="likes"> ${like} </span><i class="fas fa-heart"></i>
+          </div>
+        </div>
+      </div>
+    `;
+  });
+};
+displayMedia();
+
+const likesIncrement = (num) => {
+  const likes = document.querySelectorAll('.likes');
+  likes.forEach((like) => {
+    this.innerHTML = num++;
+  });
+};
